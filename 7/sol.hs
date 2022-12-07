1dd291747158236bee5f4e2a4eb169301f26fe41
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2022-12-07 18:06:31 -0500

    aaaa

--- 7/sol.hs
+++ 7/sol.hs
@@ -1,36 +1,84 @@
-{-# LANGUAGE ViewPatterns, ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+
+import Data.Either
+import Data.List
 import qualified Data.Map as Map
 import System.IO
-import Data.List
-import Data.Either
 
 -- newtype Vals a b = (Node a b) | b
---newtype Leaf n = Leaf n
-data Node a b = Branches a | Leaf b deriving (Show)
-data FileTree = FileTree (Node (Map.Map String FileTree) Int) deriving (Show)
+-- newtype Leaf n = Leaf n
+data Node a b
+  = Branches a
+  | Leaf b
+  deriving (Show)
 
-treeInsert :: [String] -> Int -> FileTree -> FileTree
-treeInsert paths size (FileTree (Leaf n)) = FileTree $ Leaf n
-treeInsert [new] size (FileTree (Branches tree)) =
-  FileTree $ Branches $ Map.insert new (FileTree $ Leaf size) tree
-treeInsert (dir:rest) size (FileTree (Branches tree)) =
-  FileTree $ Branches $ Map.insert dir (treeInsert rest size $ tree Map.! dir) tree
+newtype FileTree =
+  FileTree (Node (Map.Map String FileTree) Int)
+  deriving (Show)
+
+{-
+treeInsert :: [String] -> FileTree -> FileTree -> FileTree
+treeInsert paths elem (FileTree (Leaf n)) = FileTree $ Leaf n
+treeInsert [loc] elem (FileTree (Branches tree)) =
+  FileTree $ Branches $ Map.insert loc elem tree
+treeInsert (dir:rest) elem (FileTree (Branches tree)) =
+  FileTree $
+  Branches $ Map.insert dir (treeInsert rest elem $ tree Map.! dir) tree
+-}
+treeInsert :: [String] -> FileTree -> FileTree -> FileTree
+treeInsert paths elem (FileTree (Leaf n)) = FileTree $ Leaf n
+treeInsert [loc] elem (FileTree (Branches tree)) =
+  FileTree $ Branches $ Map.union tree $ Map.fromList [(loc, elem)]
+treeInsert (dir:rest) elem (FileTree (Branches tree)) =
+  FileTree $
+  Branches $ Map.insert dir (treeInsert rest elem $ tree Map.! dir) tree
 treeInsert _ _ a = a
 
 dispatch :: [String] -> [String] -> FileTree -> FileTree
 dispatch [] _ tree = tree
-dispatch ((stripPrefix "$ cd " -> Just dir):acts) path tree = 
-  if dir == ".."
-       then dispatch acts (drop 1 path) tree
-    else dispatch acts (dir:path) tree
-
+dispatch ((stripPrefix "$ cd " -> Just dir):acts) path tree =
+  dispatch acts newPath $
+  treeInsert (reverse newPath) (FileTree $ Branches Map.empty) tree
+  where
+    newPath
+      | dir == ".." = drop 1 path
+      | otherwise = dir : path
 dispatch ("$ ls":acts) path tree = dispatch acts path tree
-dispatch ((stripPrefix "dir " -> Just dir):acts) path tree = 
-  dispatch acts path tree
-dispatch (f:acts) path tree = dispatch acts path $ treeInsert path size tree
-  where size = read $ head $ words f
+dispatch ((stripPrefix "dir " -> Just dir):acts) path tree =
+  dispatch acts path $
+  treeInsert (reverse $ dir : path) (FileTree $ Branches Map.empty) tree
+dispatch (f:acts) path tree =
+  dispatch acts path $
+  treeInsert (reverse $ name : path) (FileTree $ Leaf size) tree
+  where
+    size :: Int = read $ head $ words f
+    name :: String = head . tail $ words f
 
-main :: IO()
+flattener :: FileTree -> (Int, [Int])
+flattener (FileTree (Leaf size)) = (size, [])
+flattener (FileTree (Branches dirs)) = (files, files : joined)
+  where
+    nextCall = map flattener $ Map.elems dirs
+    files = sum $ map fst nextCall
+    joined = concatMap snd nextCall
+
+-- pretty sure this is Olog(n)n^2 but we get away with it
+-- idk monads so I can't look up elements
+-- too tired
+p2 :: Int -> Int -> [Int] -> Int
+p2 n item (start:rest)
+  | item == start = n
+  | otherwise = p2 (n + 1) item rest
+p2 _ _ _ = -1
+
+main :: IO ()
 main = do
-  contents <- readFile "t.txt"
-  print $ dispatch (lines contents) [] $ FileTree $ Branches Map.empty
+  contents <- readFile "input.txt"
+  let tree = dispatch (lines contents) [] $ FileTree $ Branches Map.empty
+  let (x, flatTree) = flattener tree
+  print $ sum [n | n <- flatTree, n < 100000]
+  let freeSpace = 70000000 - head flatTree
+  let toFree = 30000000 - freeSpace
+  let smalls = [n | n <- flatTree, n > toFree]
+  print $ minimum smalls
